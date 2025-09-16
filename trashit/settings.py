8a0a311a498761fb54f94d5bfa73b29c95fb98a5
Kumfa Jina <besibangk@gmail.com>
2025-09-16 11:16:57 +0100

    Edited settings.py

--- trashit/settings.py
+++ trashit/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'trashit.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(os.getenv("DATABASE_URL"), conn_max_age=600)
+    'default': dj_database_url.parse(os.getenv("DATABASE_URL"), conn_max_age=600,ssl_require=False)
        
 }
 
